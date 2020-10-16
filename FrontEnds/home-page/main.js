e40d783c83917313b7416442bf84df9bc6721f86
Adwaith Samod <adwaithsamod@gmail.com>
2020-10-17 01:14:44 +0530

    buy page edited

---
+++ FrontEnds/home-page/main.js
@@ -0,0 +1,7 @@
+document.getElementById('buy').addEventListener('click',()  =>{
+    window.location.href="../buy/buy.html";
+});
+
+document.getElementById('sell').addEventListener('click',()  =>{
+    window.location.href="index.html";
+});

30d3a40d3f1f57515858971ad110a71a4aff28a8
Adwaith Samod <adwaithsamod@gmail.com>
2020-10-17 23:28:40 +0530

    sell page added

---
+++ FrontEnds/sell/main.js
@@ -0,0 +1,15 @@
+document.getElementById('text').addEventListener('click',()  =>{
+    window.location.href="../text/text.html";
+});
+
+document.getElementById('tool').addEventListener('click',()  =>{
+    window.location.href="../tool/tool.html";
+});
+
+document.getElementById('calc').addEventListener('click',()  =>{
+    window.location.href="../calc/calc.html";
+});
+
+document.getElementById('extra').addEventListener('click',()  =>{
+    window.location.href="../extra/extra.html";
+});

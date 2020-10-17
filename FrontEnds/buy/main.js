9df35b291dbbbbc073a1c91ee4f5ed4870efb2a1
Adwaith Samod <adwaithsamod@gmail.com>
2020-10-17 21:35:43 +0530

    buy site designed

--- FrontEnds/buy/main.js
+++ FrontEnds/buy/main.js
@@ -1,7 +1,15 @@
-document.getElementById('buy').addEventListener('click',()  =>{
-    window.location.href="./buy/index.html";
+document.getElementById('text').addEventListener('click',()  =>{
+    window.location.href="../text/text.html";
 });
 
-document.getElementById('sell').addEventListener('click',()  =>{
-    window.location.href="index.html";
+document.getElementById('tool').addEventListener('click',()  =>{
+    window.location.href="../tool/tool.html";
+});
+
+document.getElementById('calc').addEventListener('click',()  =>{
+    window.location.href="../calc/calc.html";
+});
+
+document.getElementById('extra').addEventListener('click',()  =>{
+    window.location.href="../extra/extra.html";
 });
--- FrontEnds/home-page/main.js
+++ FrontEnds/home-page/main.js
@@ -1,7 +1,7 @@
 document.getElementById('buy').addEventListener('click',()  =>{
-    window.location.href="../buy/buy.html";
+    window.location.href="../FrontEnds/buy/buy.html";
 });
 
 document.getElementById('sell').addEventListener('click',()  =>{
-    window.location.href="index.html";
+    window.location.href="../FrontEnds/sell/sell.html";
 });

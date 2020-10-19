fdc05abfc0eb325a734537a4722cfd8ab7be23e6
Adwaith Samod <adwaithsamod@gmail.com>
2020-10-20 00:51:43 +0530

    buy and sell

--- FrontEnds/home-page/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('buy').addEventListener('click',()  =>{
-    window.location.href="../FrontEnds/buy/buy.html";
-});
-
-document.getElementById('sell').addEventListener('click',()  =>{
-    window.location.href="../FrontEnds/sell/sell.html";
-});
